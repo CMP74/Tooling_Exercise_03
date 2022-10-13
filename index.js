dff482340009a27e5d4aa42e6f52ab6a943462b7
pynncm <pynncm@gmail.com>
2022-10-13 22:16:27 +1100

    Updated iFC Model and Bundler

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ viewer.grid.setGrid();
 viewer.axes.setAxes();
 
 async function loadIfc(url) {
-    await viewer.IFC.setWasmPath("../../../");
+    //await viewer.IFC.setWasmPath("../");
     const model = await viewer.IFC.loadIfcUrl(url);
     viewer.shadowDropper.renderShadow(model.modelID);
 }
-const path = "./src/SEP-084-D-SEPA-MOD-13-RWE-CSW-0102.ifc"
+const path = "./src/Drawing1.ifc"
 loadIfc(path);
 console.log(path)
 
